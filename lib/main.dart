c3f19b4216ed389c304bbcaeba0e2a25c273405f
Javier Coppis <jcoppis@grupoassa.com>
2019-06-05 18:05:05 -0300

    products with dynamic text

--- lib/main.dart
+++ lib/main.dart
@@ -2,7 +2,17 @@ import 'package:flutter/material.dart';
 
 void main() => runApp(MyApp());
 
-class MyApp extends StatelessWidget {
+class MyApp extends StatefulWidget {
+  @override
+  State<StatefulWidget> createState() {
+    // TODO: implement createState
+    return _MyAppState();
+  }
+}
+
+class _MyAppState extends State<MyApp> {
+  List<String> _products = ['Food Tester'];
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
@@ -19,14 +29,20 @@ class MyApp extends StatelessWidget {
                 child: Text('Add Product'),
               ),
             ),
-            Card(
-              child: Column(
-                children: <Widget>[
-                  Image.asset('assets/images/gatos.jpeg'),
-                  Text('Food Paradise')
-                ],
-              ),
-            ),
+            Column(
+              children: _products
+                  .map(
+                    (e) => Card(
+                          child: Column(
+                            children: <Widget>[
+                              Image.asset('assets/images/gatos.jpeg'),
+                              Text(e)
+                            ],
+                          ),
+                        ),
+                  )
+                  .toList(),
+            )
           ],
         ),
       ),
