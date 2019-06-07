a9ecc2f63da0e63627b93a492b2d7eb1c2c52090
Javier Coppis <jcoppis@grupoassa.com>
2019-06-07 14:58:21 -0300

    create the productPage

--- lib/products.dart
+++ lib/products.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 
+import 'package:first_app/pages/product.dart';
+
 class Products extends StatelessWidget {
   final List<String> products;
 
@@ -10,7 +12,20 @@ class Products extends StatelessWidget {
       child: Column(
         children: <Widget>[
           Image.asset('assets/images/gatos.jpeg'),
-          Text(products[index])
+          Text(products[index]),
+          ButtonBar(
+            alignment: MainAxisAlignment.center,
+            children: <Widget>[
+              FlatButton(
+                child: Text('Details'),
+                onPressed: () => Navigator.push(
+                      context,
+                      MaterialPageRoute(
+                          builder: (BuildContext context) => ProductPage()),
+                    ),
+              )
+            ],
+          )
         ],
       ),
     );
