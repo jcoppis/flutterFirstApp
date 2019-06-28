61282475b399d2a9b70a4ca599204ad4cfe97e19
Javier Coppis <jcoppis@grupoassa.com>
2019-06-28 17:05:42 -0300

    add tabs

---
+++ lib/pages/product_create.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+
+class ProductCreatePage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    // no retorno Scaffold porque es cargada desde un tab
+    return Center(
+      child: Text('Create a Product'),
+    );
+  }
+}
---
+++ lib/pages/product_list.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+
+class ProductListPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    // no retorno Scaffold porque es cargada desde un tab
+    return Center(
+      child: Text('All Products'),
+    );
+  }
+}
