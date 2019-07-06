33480dab465be6c3bd3018b75531b20b5abfe6c2
Javier Coppis <jcoppis@grupoassa.com>
2019-07-06 13:13:36 -0300

    add forms

--- lib/pages/auth.dart
+++ lib/pages/auth.dart
@@ -8,9 +8,12 @@ class AuthPage extends StatefulWidget {
 }
 
 class _AuthPageState extends State<AuthPage> {
-  String _emailValue;
-  String _passwordValue;
-  bool _acceptTerms = false;
+  final Map<String, dynamic> _formData = {
+    'email': null,
+    'password': null,
+    'acceptTerms': false
+  };
+  final GlobalKey<FormState> _formKey = GlobalKey<FormState>();
 
   DecorationImage _buildBackgroundImage() {
     return DecorationImage(
@@ -21,42 +24,57 @@ class _AuthPageState extends State<AuthPage> {
     );
   }
 
-  Widget _buildEmailTextField() {
-    return TextField(
+  Widget _buildEmailTextFormField() {
+    return TextFormField(
       decoration: InputDecoration(
         labelText: 'Email',
         filled: true,
         fillColor: Colors.white,
       ),
       keyboardType: TextInputType.emailAddress,
-      onChanged: (String value) {
-        setState(() {
-          _emailValue = value;
-        });
+      validator: (String value) {
+        if (value.isEmpty || !RegExp(r"[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?").hasMatch(value)) {
+          return 'Please enter a valid email';
+        }
+      },
+      onSaved: (String value) {
+          _formData['email'] = value;
       },
     );
   }
 
-  Widget _buildPasswordTextField() {
-    return TextField(
+  Widget _buildPasswordTextFormField() {
+    return TextFormField(
       decoration: InputDecoration(
         labelText: 'Password',
         filled: true,
         fillColor: Colors.white,
       ),
       obscureText: true,
-      onChanged: (String value) {
-        setState(() {
-          _passwordValue = value;
-        });
+      validator: (String value) {
+        if (value.isEmpty || value.length < 6) {
+          return 'Password invalid';
+        }
+      },
+      onSaved: (String value) {
+          _formData['password'] = value;
       },
     );
+  }
+
+  void _submitForm() {
+    if (!_formKey.currentState.validate() || !_formData['acceptTerms']) {
+      return;
+    }
+    _formKey.currentState.save();
+    Navigator.pushReplacementNamed(context, '/products');
   }
 
   @override
   Widget build(BuildContext context) {
     final double _deviceWidth = MediaQuery.of(context).size.width;
-    final double _targetWidth = _deviceWidth > 550.0 ? 500.0 : _deviceWidth * 0.95;
+    final double _targetWidth =
+        _deviceWidth > 550.0 ? 500.0 : _deviceWidth * 0.95;
     return Scaffold(
       appBar: AppBar(
         title: Text('EasyList'),
@@ -70,36 +88,35 @@ class _AuthPageState extends State<AuthPage> {
           child: SingleChildScrollView(
             child: Container(
               width: _targetWidth,
-              child: Column(
-                children: <Widget>[
-                  _buildEmailTextField(),
-                  SizedBox(
-                    height: 10.0,
-                  ),
-                  _buildPasswordTextField(),
-                  SwitchListTile(
-                    value: _acceptTerms,
-                    title: Text('Accept Terms'),
-                    onChanged: (bool value) {
-                      setState(() {
-                        _acceptTerms = value;
-                      });
-                    },
-                  ),
-                  SizedBox(
-                    height: 10.0,
-                  ),
-                  RaisedButton(
-                    color: Theme.of(context).primaryColor,
-                    textColor: Colors.white,
-                    child: Text('LOGIN'),
-                    onPressed: () {
-                      print(_emailValue);
-                      print(_passwordValue);
-                      Navigator.pushReplacementNamed(context, '/products');
-                    },
-                  ),
-                ],
+              child: Form(
+                key: _formKey,
+                child: Column(
+                  children: <Widget>[
+                    _buildEmailTextFormField(),
+                    SizedBox(
+                      height: 10.0,
+                    ),
+                    _buildPasswordTextFormField(),
+                    SwitchListTile(
+                      value: _formData['acceptTerms'],
+                      title: Text('Accept Terms'),
+                      onChanged: (bool value) {
+                        setState(() {
+                          _formData['acceptTerms'] = value;
+                        });
+                      },
+                    ),
+                    SizedBox(
+                      height: 10.0,
+                    ),
+                    RaisedButton(
+                      color: Theme.of(context).primaryColor,
+                      textColor: Colors.white,
+                      child: Text('LOGIN'),
+                      onPressed: _submitForm,
+                    ),
+                  ],
+                ),
               ),
             ),
           ),
--- lib/pages/product_create.dart
+++ lib/pages/product_create.dart
@@ -12,79 +12,100 @@ class ProductCreatePage extends StatefulWidget {
 }
 
 class _ProductCreatePageState extends State<ProductCreatePage> {
-  String _titleValue = '';
-  String _descriptionValue = '';
-  double _priceValue = 0.0;
+  final Map<String, dynamic> _formData = {
+    'title': null,
+    'description': null,
+    'price': null,
+    'image': 'assets/images/gatos.jpeg',
+  };
+  final GlobalKey<FormState> _formKey = GlobalKey<FormState>();
 
   Widget _buildTitleTextField() {
-    return TextField(
+    return TextFormField(
       decoration: InputDecoration(labelText: 'Product Title'),
-      onChanged: (String value) {
-        setState(() {
-          _titleValue = value;
-        });
+      validator: (String value) {
+        if (value.isEmpty || value.length < 5) {
+          return 'Title is required and should be 5+ characters long';
+        }
+      },
+      onSaved: (String value) {
+        _formData['title'] = value;
       },
     );
   }
 
   Widget _buildDescriptionTextField() {
-    return TextField(
+    return TextFormField(
       decoration: InputDecoration(labelText: 'Product Description'),
       maxLines: 4,
-      onChanged: (String value) {
-        setState(() {
-          _descriptionValue = value;
-        });
+      validator: (String value) {
+        if (value.isEmpty || value.length < 10) {
+          return 'Description is required and should be 10+ characters long';
+        }
+      },
+      onSaved: (String value) {
+        _formData['description'] = value;
       },
     );
   }
 
-  Widget _BuildPriceTextField() {
-    return TextField(
+  Widget _buildPriceTextField() {
+    return TextFormField(
       decoration: InputDecoration(labelText: 'Product Price'),
       keyboardType: TextInputType.number,
-      onChanged: (String value) {
-        setState(() {
-          _priceValue = double.parse(value);
-        });
+      validator: (String value) {
+        if (value.isEmpty ||
+            !RegExp(r'^(?:[1-9]\d*|0)?(?:\.\d+)?$').hasMatch(value)) {
+          return 'Price is required and should be a number';
+        }
+      },
+      onSaved: (String value) {
+        _formData['price'] = double.parse(value);
       },
     );
   }
 
   void _submitForm() {
-    final Map<String, dynamic> product = {
-      'title': _titleValue,
-      'description': _descriptionValue,
-      'price': _priceValue,
-      'image': 'assets/images/gatos.jpeg'
-    };
-    widget.addProduct(product);
+    if (!_formKey.currentState.validate()) {
+      return;
+    }
+    _formKey.currentState.save();
+    widget.addProduct(_formData);
     Navigator.pushReplacementNamed(context, '/products');
   }
 
   @override
   Widget build(BuildContext context) {
     final double _deviceWidth = MediaQuery.of(context).size.width;
-    final double _targetWidth = _deviceWidth > 550.0 ? 500.0 : _deviceWidth * 0.95;
+    final double _targetWidth =
+        _deviceWidth > 550.0 ? 500.0 : _deviceWidth * 0.95;
     final double _targetPadding = _deviceWidth - _targetWidth;
     // no retorno Scaffold porque es cargada desde un tab
-    return Container(
-      margin: EdgeInsets.all(10.0),
-      child: ListView(
-        padding: EdgeInsets.symmetric(horizontal: _targetPadding/2),
-        children: <Widget>[
-          _buildTitleTextField(),
-          _buildDescriptionTextField(),
-          _BuildPriceTextField(),
-          SizedBox(
-            height: 10.0,
+    return GestureDetector(
+      onTap: () {
+        FocusScope.of(context).requestFocus(FocusNode());
+      },
+      child: Container(
+        margin: EdgeInsets.all(10.0),
+        child: Form(
+          key: _formKey,
+          child: ListView(
+            padding: EdgeInsets.symmetric(horizontal: _targetPadding / 2),
+            children: <Widget>[
+              _buildTitleTextField(),
+              _buildDescriptionTextField(),
+              _buildPriceTextField(),
+              SizedBox(
+                height: 10.0,
+              ),
+              RaisedButton(
+                child: Text('Save'),
+                textColor: Colors.white,
+                onPressed: _submitForm,
+              )
+            ],
           ),
-          RaisedButton(
-            child: Text('Save'),
-            textColor: Colors.white,
-            onPressed: _submitForm,
-          )
-        ],
+        ),
       ),
     );
   }
