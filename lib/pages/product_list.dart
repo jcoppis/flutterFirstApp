239cfe1707db6d1c483b41e0906fbf348e2fd990
Javier Coppis <jcoppis@grupoassa.com>
2019-07-11 12:13:41 -0300

    creating Product model

--- lib/pages/product_list.dart
+++ lib/pages/product_list.dart
@@ -1,11 +1,12 @@
 import 'package:flutter/material.dart';
 
 import '../pages/product_edit.dart';
+import '../models/product.dart';
 
 class ProductListPage extends StatelessWidget {
   final Function updateProduct;
   final Function deleteProduct;
-  final List<Map<String, dynamic>> products;
+  final List<Product> products;
 
   ProductListPage(this.products, this.updateProduct, this.deleteProduct);
 
@@ -34,7 +35,7 @@ class ProductListPage extends StatelessWidget {
     return ListView.builder(
       itemBuilder: (BuildContext context, int index) {
         return Dismissible(
-          key: Key(products[index]['title']),
+          key: Key(products[index].title),
           onDismissed: (DismissDirection direction) {
             if (direction == DismissDirection.startToEnd) {
               deleteProduct(index);
@@ -48,10 +49,10 @@ class ProductListPage extends StatelessWidget {
             children: <Widget>[
               ListTile(
                 leading: CircleAvatar(
-                  backgroundImage: AssetImage(products[index]['image']),
+                  backgroundImage: AssetImage(products[index].image),
                 ),
-                title: Text(products[index]['title']),
-                subtitle: Text('\$${products[index]['price'].toString()}'),
+                title: Text(products[index].title),
+                subtitle: Text('\$${products[index].price.toString()}'),
                 trailing: _buildEditButton(context, index),
               ),
               Divider(),
